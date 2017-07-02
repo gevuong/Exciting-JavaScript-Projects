9042e5ea9d6d5caedc8e2e528e89abd0541f5145
George V <georgevuong18@gmail.com>
2017-07-02 14:32:10 -0700

    monkey-patch inject method

--- JS_Exercises/myForEach.js
+++ JS_Exercises/myForEach.js
@@ -1,3 +1,12 @@
-Array.prototype.myForEach = function() {
-  
-}
+Array.prototype.myForEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+};
+
+// callback function is essentially function (el) { console.log(el) }
+// Test Case
+const NUMS = [1, 2, 3, 4, 5];
+NUMS.myForEach(function (el) {
+  console.log(el);
+});
